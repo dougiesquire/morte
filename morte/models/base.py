58ee5fbf14b5e34d776af22f2762dc9fc162608a
dougiesquire <dougiesquire@gmail.com>
2023-01-27 15:46:29 +1100

    refactor BaseFileManifest class

--- morte/models/base.py
+++ morte/models/base.py
@@ -16,14 +16,24 @@ log_handler.setFormatter(log_format)
 logger.addHandler(log_handler)
 
 
-class BaseFileManifest(YaManifest):
+class BaseFileManifest:
     """
     Generic class for keeping track of checksums/hashes of model output files
     """
 
     def __init__(self, base_dir, reference_dir, manifest_file):
+        """
+        Initialise a BaseFileManifest object
 
-        super().__init__(manifest_file, ["binhash"])
+        Parameters
+        ----------
+        base_dir : str
+            Path to base directory of the model test experiment
+        reference_dir : str
+            Path to directory containing reference datasets (often called Known Good Outputs)
+        manifest_file : str
+            Path to manifest file containing hashes/checksums of reference datasets
+        """
 
         self.base_dir = base_dir
         self.reference_dir = reference_dir
@@ -36,6 +46,7 @@ class BaseFileManifest(YaManifest):
         # Make sure directories exists
         os.makedirs(self.reference_dir, exist_ok=True)
         os.makedirs(os.path.dirname(self.manifest_file), exist_ok=True)
+
         self.reference_files = [
             os.path.join(self.reference_dir, output) for output in self.output_files
         ]
@@ -53,20 +64,34 @@ class BaseFileManifest(YaManifest):
             for output in self.outputs_missing_references:
                 self.update_reference(output)
 
-        # Initialise the reference manifest
+        # Initialise the reference and latest manifests
+        self.reference_manifest = YaManifest(self.manifest_file, ["binhash"])
+
         if os.path.isfile(self.manifest_file):
-            self.load()
+            self.reference_manifest.load()
         else:
             logger.warning(
                 "Manifest file does not exist. Generating one from reference files"
             )
-            self.add(filepaths=self.output_files, fullpaths=self.reference_files)
-            self.dump()
+            self.reference_manifest.add(
+                filepaths=self.output_files, fullpaths=self.reference_files
+            )
+            self.reference_manifest.dump()
 
-    def compare(self):
-        """Compare hashes"""
+        # Initialise the manifest for the current experiment
+        self.current_manifest = YaManifest(None, ["binhash"])
+        self.current_manifest.add(
+            filepaths=self.output_files,
+            fullpaths=[
+                os.path.join(self.base_dir, output) for output in self.output_files
+            ],
+        )
 
     def update_reference(self, output_file):
+        """
+        Copy an output file to the reference directory. Overwrite if the file already
+        exists
+        """
 
         logger.info(f"(Over)writing reference file for {output_file}")
 
@@ -78,6 +103,16 @@ class BaseFileManifest(YaManifest):
             shutil.copy(output_file_full, reference_file)
         else:
             logger.warning(f"Output file {output_file} does not exists")
+
+    def compare(self):
+        """Compare reference and current manifests"""
+
+        return self.current_manifest.equals(self.reference_manifest, paths=False)
+
+    def dump(self):
+        """Dump reference manifest to yaml file"""
+
+        self.reference_manifest.dump()
 
 
 class BaseInfo:
